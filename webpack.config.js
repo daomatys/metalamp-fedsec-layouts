15f89e96818a64880e0df0f595393d77b97029d3
Maxim Dejnew <red41tiger@mail.ru>
2021-08-27 12:59:04 +0700

    Created first almost successfull webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
+const fs = require('fs');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development', 
-  entry: {
-    pug: './src/index.pug',
-  }
-  output: {
-    filename: '[name].bundle.pug',
-    path: path.resolve(__dirname, 'dist')
-  }
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist')
 }
+const PAGES_DIR = `${PATHS.src}/pug/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src,
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: PATHS.dist,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
 
-
+    ...PAGES.map(page => new HTMLWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+  ]
+}
