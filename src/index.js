11214d77e0df5eee09f16df49c033ff0cd6e9f0c
Maxim Dejnew <red41tiger@mail.ru>
2021-09-02 18:33:54 +0700

    After-backup fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './scss/colors.scss'
 import './scss/types.scss'
 import './scss/ui-kit-colors.scss'
-
 import './variables/variables.scss'
 
 const x = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const PATHS = {
   src: path.join(__dirname, './src'),
   dist: path.join(__dirname, './dist')
 }
-const PAGES_DIR = `${PATHS.src}/pug/`;
+const PAGES_DIR = `${PATHS.src}/pages/`;
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
