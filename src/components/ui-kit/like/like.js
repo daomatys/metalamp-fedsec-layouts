b11eeea4ef434ec8c530e24be7bc6db516393630
Maxim Dejnew <red41tiger@mail.ru>
2021-09-16 11:36:11 +0700

    Like mixin is under construction 4

--- src/components/ui-kit/like/like.js
+++ src/components/ui-kit/like/like.js
@@ -1,12 +1,28 @@
 function likeToggling() {
-  const aims = document.querySelectorAll('.like-btn');
+  const onClickEvent = ({target}) => {
+    const aim = target.closest('.like-btn'); 
 
-  for (let aim in aims) {
-    aim.onclick = () => {
-      aim.firstElementChild.textContent = 'favorite';
-      aim.classList.toggle('.like-btn_toggled');
+    if (aim) {
+      const icon = aim.querySelector('.like-btn__icon').firstElementChild;
+      const counter = aim.querySelector('.like-btn__counter').firstElementChild;
+
+      aim.classList.toggle('like-btn_toggled');
+
+      switch ( aim.classList.contains('like-btn_toggled') ) {
+        case true: {
+          icon.textContent = 'favorite'
+          ++counter.textContent;
+          break;
+        }
+        case false: {
+          icon.textContent = 'favorite_border';
+          --counter.textContent;
+          break;
+        }
+      }
     }
-  }  
+  }
+  document.querySelector('.like-btn__elem').addEventListener('click', onClickEvent)
 }
 
 likeToggling();
