5531c2b7507ef0760de8f852c19c180ad253b3c4
Maxim Dejnew <red41tiger@mail.ru>
2021-09-17 12:00:26 +0700

    buttons section is under construction 1

--- src/components/buttons/like/like.js
+++ src/components/buttons/like/like.js
@@ -1,5 +1,5 @@
 function likeButtonEventListeners() {
-  const items = document.querySelectorAll('.like-mode');
+  const items = document.querySelectorAll('.like-button');
 
   for (let item of items) {
     item.addEventListener('click', likeButtonPressing);
@@ -7,14 +7,14 @@ function likeButtonEventListeners() {
 }
 
 function likeButtonPressing({target}) {
-  const aim = target.closest('.like-mode'); 
-  const icon = aim.querySelector('.like-mode__icon').firstElementChild;
-  const counter = aim.querySelector('.like-mode__counter').firstElementChild;
+  const aim = target.closest('.like-button'); 
+  const icon = aim.querySelector('.like-button__icon').firstElementChild;
+  const counter = aim.querySelector('.like-button__counter').firstElementChild;
 
-  aim.classList.toggle('micro-button_pressed');
+  aim.classList.toggle('like-button_pressed');
   icon.classList.toggle('gradient-fill');
 
-  switch ( aim.classList.contains('micro-button_pressed') ) {
+  switch ( aim.classList.contains('like-button_pressed') ) {
     case true: {
       icon.textContent = 'favorite'
       ++counter.textContent;
