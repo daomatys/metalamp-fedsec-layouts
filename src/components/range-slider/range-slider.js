9cddc4f8155477555d74cb90019628fc5ac96ead
Maxim Dejnew <red41tiger@mail.ru>
2021-09-22 20:32:13 +0700

    Range-slider is under construction 2

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,6 +1,7 @@
 $( 
   function() {
     $( "#range-slider" ).slider({
+      animate: "fast",
       range: true,
       min: 1000,
       max: 15000,
@@ -8,15 +9,15 @@ $(
       values: [ 5000, 10000 ],
       stop: function( event, ui ) {
         $( "#range-slider-marker" ).text(
-          ui.values[ 0 ].toLocaleString('ru-RU') + "₽ - " +
-          ui.values[ 1 ].toLocaleString('ru-RU') + "₽"
+          ui.values[ 0 ].toLocaleString("ru-RU") + "₽ - " +
+          ui.values[ 1 ].toLocaleString("ru-RU") + "₽"
         );
       }
     });
 
     $( "#range-slider-marker" ).text(
-      $( "#range-slider" ).slider( "values", 0 ).toLocaleString('ru-RU') + "₽ - " + 
-      $( "#range-slider" ).slider( "values", 1 ).toLocaleString('ru-RU') + "₽"
+      $( "#range-slider" ).slider( "values", 0 ).toLocaleString("ru-RU") + "₽ - " + 
+      $( "#range-slider" ).slider( "values", 1 ).toLocaleString("ru-RU") + "₽"
     );
   }
 );
