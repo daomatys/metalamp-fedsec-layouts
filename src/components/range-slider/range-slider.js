d6e830e4ea5a1892174f7845fc692d615e60a597
Maxim Dejnew <red41tiger@mail.ru>
2021-09-22 19:02:43 +0700

    Refactored title-bar and range-slider for merging each other into appropriate construction

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -3,13 +3,20 @@ $(
     $( "#range-slider" ).slider({
       range: true,
       min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      max: 16000,
+      step: 100,
+      values: [ 5000, 10000 ],
+      stop: function( event, ui ) {
+        $( "#range-slider-marker" ).text(
+          ui.values[ 0 ].toLocaleString('ru-RU') + "₽ - " +
+          ui.values[ 1 ].toLocaleString('ru-RU') + "₽"
+        );
       }
     });
 
-    $( "#amount" ).val( "$" + $( "#range-slider" ).slider( "values", 0 ) + " - $" + $( "#range-slider" ).slider( "values", 1 ) );
+    $( "#range-slider-marker" ).text(
+      $( "#range-slider" ).slider( "values", 0 ) + "₽ - " + 
+      $( "#range-slider" ).slider( "values", 1 ) + "₽"
+    );
   }
 );
