c6f46d4a3b359c63a761d138a5aa4f546e160d92
Maxim Dejnew <red41tiger@mail.ru>
2021-10-06 19:43:20 +0700

    Added input_date-picker scss and js docs

---
+++ src/components/input/_date-picker/input_date-picker.js
@@ -0,0 +1,45 @@
+import 'jquery-ui/ui/widgets/datepicker';
+
+$.datepicker.regional['ru'] = {
+	closeText: 'Закрыть',
+	prevText: 'Предыдущий',
+	nextText: 'Следующий',
+	currentText: 'Сегодня',
+	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+	weekHeader: 'Не',
+	dateFormat: 'dd.mm.yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''
+};
+$.datepicker.setDefaults($.datepicker.regional['ru']);
+
+function findDatePickerContainer() {
+  const datePickerItems = document.querySelectorAll(".date-picker");
+
+  console.log(datePickerItems)
+
+  if ( datePickerItems ) {
+    for ( let item of datePickerItems ) {
+      initDatePicker( item );
+    }
+  }
+}
+
+function initDatePicker( item ) {
+  const itemId = '#' + item.id;
+
+  $( itemId ).datepicker({
+		showOn: "button",
+		buttonImage: "https://snipp.ru/demo/437/calendar.gif",
+		buttonImageOnly: true,
+		buttonText: "Выбрать дату"
+  });
+}
+
+findDatePickerContainer();
