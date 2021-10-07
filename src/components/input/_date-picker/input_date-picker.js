e8088a1421490d5e2ba63a926759a186bb6ac752
Maxim Dejnew <red41tiger@mail.ru>
2021-10-07 11:38:17 +0700

    Input_date-picker is under construction

--- src/components/input/_date-picker/input_date-picker.js
+++ src/components/input/_date-picker/input_date-picker.js
@@ -1,21 +1,33 @@
 import 'jquery-ui/ui/widgets/datepicker';
 
+const DATENAMES = {
+  months: {
+    full:  ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    short: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек']
+  },
+  days: {
+    full:  ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+    short: ['вс','пн','вт','ср','чт','пт','сб'],
+    min:   ['Вс','Пн','Вт','Ср','Чт','Пт','Сб']
+  }
+}
+
 $.datepicker.regional['ru'] = {
-	closeText: 'Закрыть',
-	prevText: 'Предыдущий',
-	nextText: 'Следующий',
-	currentText: 'Сегодня',
-	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-	weekHeader: 'Не',
-	dateFormat: 'dd.mm.yy',
-	firstDay: 1,
-	isRTL: false,
-	showMonthAfterYear: false,
-	yearSuffix: ''
+  closeText:   'Закрыть',
+  prevText:    'Предыдущий месяц',
+  nextText:    'Следующий месяц',
+  currentText: 'Сегодня',
+  monthNames:      DATENAMES.months.full,
+  monthNamesShort: DATENAMES.months.short,
+  dayNames:        DATENAMES.days.full,
+  dayNamesShort:   DATENAMES.days.short,
+  dayNamesMin:     DATENAMES.days.min,
+  weekHeader:  'Не',
+  dateFormat:  'dd.mm.yy',
+  yearSuffix:  '',
+  firstDay: 1,
+  isRTL:              false,
+  showMonthAfterYear: false
 };
 
 $.datepicker.setDefaults($.datepicker.regional['ru']);
@@ -37,7 +49,7 @@ function initDatePicker( item ) {
 
   $( itemId ).datepicker({
     showOtherMonths: true,
-		buttonText: "Выбрать дату"
+    buttonText: "Выбрать дату"
   });
 }
 
