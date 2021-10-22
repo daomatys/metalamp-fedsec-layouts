08fbef752472e8c99d82498e212f090ac93dc8d7
Maxim Dejnew <red41tiger@mail.ru>
2021-10-23 01:01:32 +0700

    Modifying votes-chart element 9

--- src/components/votes-chart/votes-chart.js
+++ src/components/votes-chart/votes-chart.js
@@ -1,10 +1,21 @@
 const init = function initEventListeners() {
   const arcs = document.querySelectorAll('.votes-chart__diagram-element_unit');
+  const legendItems = document.querySelectorAll('.votes-chart__legend-item');
 
   arcs.forEach( arc => arc.addEventListener('focus', () => onFocus( arc )) );
+  legendItems.forEach( item => item.addEventListener('pointerdown', (event) => onLegendClick( event, item )) );
 }
 
-const onFocus = function onFocusEvent(arc) {
+const onLegendClick = function onLegendClickEvent( event, item ) {
+  event.preventDefault();
+
+  const relatedID = '#' + item.getAttribute('data-link');
+  const aim = document.querySelector( relatedID );
+
+  aim.focus();
+}
+
+const onFocus = function onFocusEvent( arc ) {
   const defineElement = suffix => document.querySelector(`.votes-chart__diagram-text_${suffix}`);
 
   const centralTextVotes = defineElement('votes');
@@ -20,7 +31,7 @@ const onFocus = function onFocusEvent(arc) {
   const newWidthValue = 32;
 
   const apply = function applyProperties( vvalue, vvote, wword ) {
-    const switchState = function switchElementsContentAndClasses(elem, text) {
+    const switchState = function switchElementsContentAndClasses( elem, text ) {
       elem.textContent = text;
       elem.classList.toggle(`gradient__${arc.id.slice(12)}_end`);
     }
