0f426e158ee0bf3f0f77041a4e8ca5b71bd685a2
Maxim Dejnew <red41tiger@mail.ru>
2021-10-23 00:00:26 +0700

    Modifying votes-chart element 7

--- src/components/votes-chart/votes-chart.js
+++ src/components/votes-chart/votes-chart.js
@@ -1,42 +1,39 @@
-const votes = function() {
+const init = function initEventListeners() {
   const arcs = document.querySelectorAll('.votes-chart__diagram-element_unit');
 
-  arcs.forEach( arc => arc.addEventListener('pointerover', onPointerOver ) );
+  arcs.forEach( arc => arc.addEventListener('focus', () => onFocus( arc ), { once: true }) );
 }
 
-const onPointerOver = function pointerOverEvent({target}) {
+const onFocus = function onFocusEvent(arc) {
   const defineElement = suffix => document.querySelector(`.votes-chart__diagram-text_${suffix}`);
 
   const centralTextVotes = defineElement('votes');
   const centralTextWord = defineElement('word');
 
   const votesTotal = centralTextVotes.textContent;
-  const votes = target.getAttribute('data-votes');
+  const votes = arc.getAttribute('data-votes');
 
   const wordOriginal = centralTextWord.textContent;
   const word = 'голос' + wordEnding( votes );
 
-  const lastWidthValue = target.getAttribute('stroke-width');
+  const lastWidthValue = arc.getAttribute('stroke-width');
   const newWidthValue = 32;
 
   const apply = function applyProperties( vvalue, vvote, wword ) {
-    const switchState = (elem, text) => {
+    const switchState = function switchElementsContentAndClasses(elem, text) {
       elem.textContent = text;
-      elem.classList.toggle( `gradient__${target.id.slice(12)}_end` );
+      elem.classList.toggle(`gradient__${arc.id.slice(12)}_end`);
     }
-
     switchState( centralTextVotes, vvote );
     switchState( centralTextWord, wword );
 
-    target.setAttribute('stroke-width', vvalue);
+    arc.setAttribute('stroke-width', vvalue);
   }
 
   apply( newWidthValue, votes, word );
 
-  target.addEventListener('pointerout', () => apply( lastWidthValue, votesTotal, wordOriginal ), {once: true});
+  arc.addEventListener('blur', () => apply( lastWidthValue, votesTotal, wordOriginal ), { once: true });
 }
-
-
 
 const wordEnding = function defineWordEndingAccordingToNumber( num ) {
   let result = ""
@@ -51,4 +48,4 @@ const wordEnding = function defineWordEndingAccordingToNumber( num ) {
   return result;
 }
 
-votes();
+init();
