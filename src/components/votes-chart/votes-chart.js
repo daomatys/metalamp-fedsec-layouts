3c06db335a053478b3233afc6f15fabd39bb3c54
Maxim Dejnew <red41tiger@mail.ru>
2021-10-22 21:19:28 +0700

    Modifying votes-chart element 5

--- src/components/votes-chart/votes-chart.js
+++ src/components/votes-chart/votes-chart.js
@@ -4,17 +4,22 @@ const votes = function() {
   arcs.forEach( arc => arc.addEventListener('pointerover', onPointerOver ) );
 }
 
-const onPointerOver = function XXZ({target}) {
+const onPointerOver = function pointerOverEvent({target}) {
+  const centralTextValue = document.querySelector('.votes-chart__diagram-text_votes');
+  const centralText = centralTextValue.textContent;
+
   const lastWidthValue = target.getAttribute('stroke-width');
-  const newWidthValue = 36;
+  const newWidthValue = 32;
 
   target.setAttribute('stroke-width', newWidthValue);
+  centralTextValue.textContent = target.getAttribute('data-votes');
 
-  target.addEventListener('pointerout', () => onPointerOut( target, lastWidthValue ), {once: true});
+  target.addEventListener('pointerout', () => onPointerOut( target, lastWidthValue, centralText ), {once: true});
 }
 
-const onPointerOut = function ASFSA( target, lastWidthValue ) {
+const onPointerOut = function pointerOutEvent( target, lastWidthValue, centralText ) {
   target.setAttribute('stroke-width', lastWidthValue);
+  document.querySelector('.votes-chart__diagram-text_votes').textContent = centralText;
 }
 
 votes();
