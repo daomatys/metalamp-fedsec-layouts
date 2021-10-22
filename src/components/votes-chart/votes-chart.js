93c2422ebc6ea75cf8fccfe3b75602aa45081a52
Maxim Dejnew <red41tiger@mail.ru>
2021-10-22 22:06:17 +0700

    Modifying votes-chart element 6

--- src/components/votes-chart/votes-chart.js
+++ src/components/votes-chart/votes-chart.js
@@ -5,21 +5,50 @@ const votes = function() {
 }
 
 const onPointerOver = function pointerOverEvent({target}) {
-  const centralTextValue = document.querySelector('.votes-chart__diagram-text_votes');
-  const centralText = centralTextValue.textContent;
+  const defineElement = suffix => document.querySelector(`.votes-chart__diagram-text_${suffix}`);
+
+  const centralTextVotes = defineElement('votes');
+  const centralTextWord = defineElement('word');
+
+  const votesTotal = centralTextVotes.textContent;
+  const votes = target.getAttribute('data-votes');
+
+  const wordOriginal = centralTextWord.textContent;
+  const word = 'голос' + wordEnding( votes );
 
   const lastWidthValue = target.getAttribute('stroke-width');
   const newWidthValue = 32;
 
-  target.setAttribute('stroke-width', newWidthValue);
-  centralTextValue.textContent = target.getAttribute('data-votes');
+  const apply = function applyProperties( vvalue, vvote, wword ) {
+    const switchState = (elem, text) => {
+      elem.textContent = text;
+      elem.classList.toggle( `gradient__${target.id.slice(12)}_end` );
+    }
 
-  target.addEventListener('pointerout', () => onPointerOut( target, lastWidthValue, centralText ), {once: true});
+    switchState( centralTextVotes, vvote );
+    switchState( centralTextWord, wword );
+
+    target.setAttribute('stroke-width', vvalue);
+  }
+
+  apply( newWidthValue, votes, word );
+
+  target.addEventListener('pointerout', () => apply( lastWidthValue, votesTotal, wordOriginal ), {once: true});
 }
 
-const onPointerOut = function pointerOutEvent( target, lastWidthValue, centralText ) {
-  target.setAttribute('stroke-width', lastWidthValue);
-  document.querySelector('.votes-chart__diagram-text_votes').textContent = centralText;
+
+
+const wordEnding = function defineWordEndingAccordingToNumber( num ) {
+  let result = ""
+
+  if ( Math.floor(num/10)===1 || num%10<1 || num%10>4 ) {
+    result = "ов";
+ } else {
+    if ( num%10>1 ) {
+      result = "a";
+   }
+ }
+  return result;
 }
 
 votes();
