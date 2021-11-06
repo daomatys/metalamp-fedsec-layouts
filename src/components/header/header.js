ab7de1c6a1c8d3998ec978e423310ac03a713ba8
Maxim Dejnew <red41tiger@mail.ru>
2021-11-06 18:46:57 +0700

    Modal-window and its implementation is under construction 2

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-const init = function() {
+const initEjector = function() {
   const ejectors = document.querySelectorAll(".header__ejector");
 
   ejectors.forEach( ejector => ejector.addEventListener("click", ({target}) => onEjectorClick(target)) );
@@ -12,4 +12,28 @@ const onEjectorClick = function onEjectorClickEvent( target ) {
   }
 }
 
-init();
+initEjector();
+
+
+const initButtons = function initButtons() {
+  const windowClassListToggle = window => {
+    window.classList.toggle('.modal-window_visible');
+    window.classList.toggle('.modal-window-invisible');
+  }
+  const defineButon = suffix => document.querySelector(`#ejectable-button_${suffix}-1`);
+  const defineButtonAction = ( button, window ) => button.addEventListener('click', () => windowClassListToggle( window ) );
+
+  const buttonFilter = defineButon('filter-room');
+  const buttonNavigation = defineButon('navigation');
+  const buttonAuthorization = defineButon('authorization');
+
+  const modalWindowFilter = document.querySelector('.search-page__input-parameters');
+  const modalWindowNavigation = document.querySelector('.modal-window_navigation');
+  const modalWindowAuthorization = document.querySelector('.modal-window_authorization');
+
+  defineButtonAction( buttonFilter,        modalWindowFilter );
+  defineButtonAction( buttonNavigation,    modalWindowNavigation );
+  defineButtonAction( buttonAuthorization, modalWindowAuthorization );
+}
+
+initButtons();
