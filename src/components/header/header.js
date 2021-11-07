041b366ebe4f61b17df0cd3a7b430384a3a6ecca
Maxim Dejnew <red41tiger@mail.ru>
2021-11-07 17:41:01 +0700

    Fixed bug with same ids of two different card filter items

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,32 +17,35 @@ initEjector();
 
 const initButtons = function initButtons() {
   const main = document.querySelector('.website__main');
-  const footer = document.querySelector('.website__footer');
 
-  const elementClassListToggle = element => {
-    element.classList.toggle('window_invisible');
+  if ( main ) {
+    const footer = document.querySelector('.website__footer');
+
+    const elementClassListToggle = element => {
+      element.classList.toggle('window_invisible');
+    }
+
+    const windowsClassListToggle = window => {
+      elementClassListToggle( main );
+      elementClassListToggle( window );
+      elementClassListToggle( footer );
+    }
+
+    const defineButon = suffix => document.querySelector(`#ejectable-button_${suffix}-1`);
+    const defineButtonAction = ( button, window ) => button.addEventListener('click', () => windowsClassListToggle( window ) );
+
+    const buttonFilter = defineButon('filtration');
+    const buttonNavigation = defineButon('navigation');
+    const buttonAuthorization = defineButon('authorization');
+
+    const modalWindowFilter = document.querySelector('.modal-window_filtration');
+    const modalWindowNavigation = document.querySelector('.modal-window_navigation');
+    const modalWindowAuthorization = document.querySelector('.modal-window_authorization');
+
+    if ( modalWindowFilter ) defineButtonAction( buttonFilter, modalWindowFilter );
+    defineButtonAction( buttonNavigation,    modalWindowNavigation );
+    defineButtonAction( buttonAuthorization, modalWindowAuthorization );
   }
-
-  const windowsClassListToggle = window => {
-    elementClassListToggle( main );
-    elementClassListToggle( window );
-    elementClassListToggle( footer );
-  }
-
-  const defineButon = suffix => document.querySelector(`#ejectable-button_${suffix}-1`);
-  const defineButtonAction = ( button, window ) => button.addEventListener('click', () => windowsClassListToggle( window ) );
-
-  const buttonFilter = defineButon('filtration');
-  const buttonNavigation = defineButon('navigation');
-  const buttonAuthorization = defineButon('authorization');
-
-  const modalWindowFilter = document.querySelector('.modal-window_filtration');
-  const modalWindowNavigation = document.querySelector('.modal-window_navigation');
-  const modalWindowAuthorization = document.querySelector('.modal-window_authorization');
-
-  if ( modalWindowFilter ) defineButtonAction( buttonFilter, modalWindowFilter );
-  defineButtonAction( buttonNavigation,    modalWindowNavigation );
-  defineButtonAction( buttonAuthorization, modalWindowAuthorization );
 }
 
 initButtons();
