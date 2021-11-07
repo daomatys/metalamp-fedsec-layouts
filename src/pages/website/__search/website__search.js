3b951d6c3259d3e06eab1bc7293c666c97d6f9de
Maxim Dejnew <red41tiger@mail.ru>
2021-11-07 15:12:44 +0700

    Fixed website search script

--- src/pages/website/__search/website__search.js
+++ src/pages/website/__search/website__search.js
@@ -2,7 +2,7 @@ const init = function init() {
   const activator = document.querySelector('.search-page__shift-activator');
 
   if ( activator ) {
-    activator.onclick = activator.closest('.search-page__input-parameters-block').classList.toggle('window-shifted');
+    activator.onclick = () => activator.closest('.search-page__input-parameters-block').classList.toggle('window_shifted');
   }
 }
 
