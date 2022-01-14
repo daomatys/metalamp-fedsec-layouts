50570e43152689301140ccf324b054dd6a16f5b2
Maxim Dejnew <red41tiger@mail.ru>
2022-01-14 22:03:12 +0700

    linted(fix): pages volume

--- src/pages/website/__search/website__search.js
+++ src/pages/website/__search/website__search.js
@@ -5,13 +5,12 @@ import './website__search.scss';
 import '@components/pagination/pagination';
 import '@components/card/__filter/card__filter';
 
-
 const init = function init() {
   const activator = document.querySelector('.search-page__shift-activator');
 
-  if ( activator ) {
+  if (activator) {
     activator.onclick = () => activator.closest('.search-page__input-parameters-block').classList.toggle('window_shifted');
   }
-}
+};
 
 init();
