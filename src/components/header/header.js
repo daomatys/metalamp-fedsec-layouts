7a61c8f1cc0be4f13a289c766ef882df0c7f8efd
Maxim Dejnew <red41tiger@mail.ru>
2022-01-14 21:35:11 +0700

    lint: header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,43 +5,41 @@ import '@components/material-icon-cell/material-icon-cell';
 import '@components/navigation-bar/navigation-bar';
 import '@components/button/__mean-oval/button__mean-oval';
 
+const initEjector = function () {
+  const ejectors = document.querySelectorAll('.header__ejector');
 
-const initEjector = function() {
-  const ejectors = document.querySelectorAll(".header__ejector");
+  ejectors.forEach((ejector) => ejector.addEventListener('click', ({ target }) => onEjectorClick(target)));
+};
 
-  ejectors.forEach( ejector => ejector.addEventListener("click", ({target}) => onEjectorClick(target)) );
-}
+const onEjectorClick = function onEjectorClickEvent(target) {
+  const aim = target.closest('.header__ejector');
 
-const onEjectorClick = function onEjectorClickEvent( target ) {
-  const aim = target.closest(".header__ejector");
-
-  if ( aim ) {
-    aim.closest(".header__container_ejectable").classList.toggle("container-ejected");
+  if (aim) {
+    aim.closest('.header__container_ejectable').classList.toggle('container-ejected');
   }
-}
+};
 
 initEjector();
-
 
 const initButtons = function initButtons() {
   const main = document.querySelector('.website__main');
 
-  if ( main ) {
+  if (main) {
     const footer = document.querySelector('.website__footer');
 
-    const elementClassListToggle = element => {
+    const elementClassListToggle = function elementClassListToggle(element) {
       element.classList.toggle('modal-window_invisible');
-    }
-    const windowsClassListToggle = function windowsClassListToggle( window ) {
-      elementClassListToggle( main );
-      elementClassListToggle( window );
-      elementClassListToggle( footer );
-    }
-    const defineButtonAction = function defineButtonActionForCertainModalWindow( button, window ) {
-      button.onclick = () => windowsClassListToggle( window );
-    }
-    const defineButon = suffix => document.querySelector(`#ejectable-button_${suffix}-1`);
-    const defineModalWindow = suffix => document.querySelector(`.modal-window__${suffix}`);
+    };
+    const windowsClassListToggle = function windowsClassListToggle(window) {
+      elementClassListToggle(main);
+      elementClassListToggle(window);
+      elementClassListToggle(footer);
+    };
+    const defineButtonAction = function defineButtonActionForCertainModalWindow(button, window) {
+      button.onclick = () => windowsClassListToggle(window);
+    };
+    const defineButon = (suffix) => document.querySelector(`#ejectable-button_${suffix}-1`);
+    const defineModalWindow = (suffix) => document.querySelector(`.modal-window__${suffix}`);
 
     const buttonFiltration = defineButon('filtration');
     const buttonNavigation = defineButon('navigation');
@@ -51,12 +49,12 @@ const initButtons = function initButtons() {
     const modalWindowNavigation = defineModalWindow('navigation');
     const modalWindowAuthorization = defineModalWindow('authorization');
 
-    if ( modalWindowFiltration ) {
-      defineButtonAction( buttonFiltration, modalWindowFiltration );
+    if (modalWindowFiltration) {
+      defineButtonAction(buttonFiltration, modalWindowFiltration);
     }
-    defineButtonAction( buttonNavigation,    modalWindowNavigation );
-    defineButtonAction( buttonAuthorization, modalWindowAuthorization );
+    defineButtonAction(buttonNavigation, modalWindowNavigation);
+    defineButtonAction(buttonAuthorization, modalWindowAuthorization);
   }
-}
+};
 
 initButtons();
