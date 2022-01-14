ca4adbfe4e4ea1bcc138329715db45baf264578a
Maxim Dejnew <red41tiger@mail.ru>
2022-01-14 22:02:39 +0700

    linted(fix): entities volume

--- src/entities/__expandable/entities__expandable.js
+++ src/entities/__expandable/entities__expandable.js
@@ -1,43 +1,42 @@
 import './entities__expandable.scss';
 
-
 const init = function expanderInitEventListeners() {
   const expanders = document.querySelectorAll('.js-expander');
 
-  if ( expanders ) {
-    expanders.forEach( expander => expander.addEventListener('click', () => activate( expander )) );
+  if (expanders) {
+    expanders.forEach((expander) => expander.addEventListener('click', () => activate(expander)));
   }
-}
+};
 
-const activate = function expanderActivation( expander ) {
+const activate = function expanderActivation(expander) {
   const parent = expander.closest('.js-expander__parent');
   const aim = parent.querySelector('.js-expander__aim');
   const icon = parent.querySelector('.material-icons');
   const caseExpanderIsDisabled = expander.hasAttribute('checked');
 
-  if ( caseExpanderIsDisabled ) {
+  if (caseExpanderIsDisabled) {
     expander.removeAttribute('checked');
   } else {
     expander.setAttribute('checked', '');
   }
 
-  animate( icon );
-  
-  if ( aim ) {
+  animate(icon);
+
+  if (aim) {
     aim.classList.toggle('js-expander_active');
   }
-}
+};
 
-const animate = function iconAnimation( icon ) {
+const animate = function iconAnimation(icon) {
   const animation = icon.animate({
-    transform: 'rotateX(0.5turn)'
+    transform: 'rotateX(0.5turn)',
   }, {
     easing: 'ease',
     duration: 400,
     fill: 'both',
-    composite: 'add'
+    composite: 'add',
   });
   animation.persist();
-}
+};
 
 init();
