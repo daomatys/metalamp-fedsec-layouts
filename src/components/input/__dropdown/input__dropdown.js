043680ebebabad09906d03cde095df871367e732
Maxim Dejnew <red41tiger@mail.ru>
2022-01-15 14:26:23 +0700

    linted: input block

--- src/components/input/__dropdown/input__dropdown.js
+++ src/components/input/__dropdown/input__dropdown.js
@@ -5,66 +5,22 @@ import '@components/material-icon-cell/material-icon-cell';
 import '@components/button/__mean-oval/button__mean-oval';
 import '@components/button/__circle/button__circle';
 
-const find = function findDropdownContainers() {
-  const dropdowns = document.querySelectorAll('.input__dropdown');
+function acceptForm(controller) {
+  const frame = controller.closest('.input__element').querySelector('.input__frame');
 
-  dropdowns.forEach((dropdown) => initListeners(dropdown));
-};
+  frame.dispatchEvent(new Event('click'));
+}
 
-const initListeners = function initElementsEventListeners(dropdown) {
-  const counters = dropdown.querySelectorAll('.input__option-counter');
-  const buttonGroups = dropdown.querySelectorAll('.input__option-button-block');
-  const controllersBar = dropdown.querySelector('.input__state-controllers');
+function defineCountersSum(counters) {
+  const result = Array
+    .from(counters)
+    .map((counter) => Number(counter.textContent))
+    .reduce((acc, curr) => acc + curr);
 
-  updateDropdownValue(Array.from(counters)[0], counters);
+  return result;
+}
 
-  if (controllersBar) {
-    const controllers = defineButtons(controllersBar);
-
-    initControllers(counters, controllers, buttonGroups);
-    buttonGroups.forEach((buttons) => initButtons(buttons, counters, controllers));
-  }
-
-  if (!controllersBar) {
-    buttonGroups.forEach((buttons) => initButtons(buttons, counters, undefined));
-  }
-};
-
-// init controllers section
-
-const initControllers = function initControllersEventListeners(counters, controllers, buttonGroups) {
-  const resetController = controllers.left;
-  const acceptController = controllers.right;
-
-  defineResetControllerState(counters, controllers);
-
-  resetController.addEventListener('click', () => resetCounters(controllers, counters, buttonGroups));
-  acceptController.addEventListener('click', () => acceptForm(acceptController));
-};
-
-const defineResetControllerState = function (counters, controllers) {
-  const countersSum = defineCountersSum(counters);
-
-  adjustButtonsState(controllers, countersSum, 'js-mean-oval-button_hidden', true);
-};
-
-// init buttons section
-
-const initButtons = function initOptionButtonsEventListeners(buttons, counters, controllers) {
-  const optionButtons = defineButtons(buttons);
-  const optionCounter = buttons.querySelector('.input__option-counter');
-  const optionCounterValue = optionCounter.textContent;
-
-  adjustButtonsState(optionButtons, optionCounterValue, 'circle-button_frozen');
-
-  const update = function (elem, shift) {
-    elem.onclick = () => updateDropdown(optionButtons, optionCounter, shift, controllers, counters);
-  };
-  update(optionButtons.left, -1);
-  update(optionButtons.right, 1);
-};
-
-const defineButtons = function defineFirstAndLastChildByItsParent(parent) {
+function defineButtons(parent) {
   const firstElement = parent.firstElementChild;
   const lastElement = parent.lastElementChild;
 
@@ -73,23 +29,89 @@ const defineButtons = function defineFirstAndLastChildByItsParent(parent) {
     right: lastElement,
   };
   return result;
-};
+}
 
-// general dropdown state-update functions
+function adjustButtonsState(buttons, value, selector, leftOnly) {
+  const caseA = value < 1;
+  const caseB = value > 9;
 
-const updateDropdown = function updateCalcsAndControllersToCurrentState(buttons, counter, addification, controllers, counters) {
-  const newCounterValue = parseInt(counter.textContent) + addification;
-
-  adjustButtonsState(buttons, newCounterValue, 'circle-button_frozen');
-  counter.textContent = newCounterValue;
-  updateDropdownValue(counter, counters);
-
-  if (controllers) {
-    defineResetControllerState(counters, controllers);
+  if (caseA || caseB) {
+    if (caseA) {
+      buttons.left.classList.add(selector);
+    }
+    if (caseB && !leftOnly) {
+      buttons.right.classList.add(selector);
+    }
   }
-};
+  if (!(caseA || caseB)) {
+    buttons.left.classList.remove(selector);
+    buttons.right.classList.remove(selector);
+  }
+}
 
-const updateDropdownValue = function updateDropdownValue(anychild, counters) {
+function defineResetControllerState(counters, controllers) {
+  const countersSum = defineCountersSum(counters);
+
+  adjustButtonsState(controllers, countersSum, 'js-mean-oval-button_hidden', true);
+}
+
+function defineEnding(currentValue, endings) {
+  const caseA = Math.floor(currentValue / 10) === 1;
+  const caseB = currentValue % 10 < 1;
+  const caseC = currentValue % 10 > 4;
+
+  const caseAppropriateRange = caseA || caseB || caseC;
+  const caseNotDividerOfTen = currentValue % 10 > 1;
+
+  if (caseAppropriateRange) {
+    const i = 1;
+    return endings[i];
+  }
+  if (caseNotDividerOfTen) {
+    const i = 2;
+    return endings[i];
+  }
+  const i = 0;
+  return endings[i];
+}
+
+function defineText(currentValue, lastValue, word, endings = []) {
+  const fixedLastValue = lastValue > 0 ? ', ' : '';
+  const fixedCurrentValue = `${currentValue} `;
+  const ending = defineEnding(currentValue, endings);
+
+  if (currentValue > 0) {
+    const textParts = [
+      fixedLastValue,
+      fixedCurrentValue,
+      word,
+      ending,
+    ];
+    return textParts.join('');
+  }
+  return '';
+}
+
+function defineGuestInputValue(values) {
+  const valuesSum = values.reduce((acc, curr) => acc + curr);
+
+  const firstText = defineText(valuesSum, 0, 'гост', ['ь', 'ей', 'я']);
+  const lastText = defineText(values[2], valuesSum, 'младен', ['ец', 'цев', 'ца']);
+
+  return firstText + lastText;
+}
+
+function defineFacilitiesInputValue(values) {
+  const firstText = defineText(values[0], 0, 'cпал', ['ьня', 'ен', 'ьни']);
+  const secondText = defineText(values[1], values[0], 'кроват', ['ь', 'ей', 'и']);
+  const thirdText = defineText(values[2], values[0] + values[1], 'ванн', ['ая комната', 'ых комнат', 'ые комнаты']);
+
+  const result = `${firstText + secondText + thirdText}…`;
+
+  return result.length < 2 ? '' : result;
+}
+
+function updateDropdownValue(anychild, counters) {
   const dropdown = anychild.closest('.input__dropdown');
   const frame = dropdown.parentNode.querySelector('.input__frame');
   const values = Array.from(counters, (item) => Number(item.textContent));
@@ -106,76 +128,18 @@ const updateDropdownValue = function updateDropdownValue(anychild, counters) {
       dropdownValue = defineFacilitiesInputValue(values);
       break;
     }
+    default: {
+      break;
+    }
   }
   frame.value = dropdownValue;
-};
+}
 
-const defineGuestInputValue = function defineGuestInputValue(values) {
-  const valuesSum = values.reduce((acc, curr) => acc + curr);
-
-  const firstText = defineText(valuesSum, 0, 'гост', ['ь', 'ей', 'я']);
-  const lastText = defineText(values[2], valuesSum, 'младен', ['ец', 'цев', 'ца']);
-
-  return firstText + lastText;
-};
-
-const defineFacilitiesInputValue = function defineFacilitiesInputValue(values) {
-  const firstText = defineText(values[0], 0, 'cпал', ['ьня', 'ен', 'ьни']);
-  const secondText = defineText(values[1], values[0], 'кроват', ['ь', 'ей', 'и']);
-  const thirdText = defineText(values[2], values[0] + values[1], 'ванн', ['ая комната', 'ых комнат', 'ые комнаты']);
-
-  const result = `${firstText + secondText + thirdText}…`;
-
-  return result.length < 2 ? '' : result;
-};
-
-const defineText = function defineTextAccordingToNumber(currentValue, lastValue, word, endings = []) {
-  let result = '';
-  let ending = endings[0];
-
-  if (currentValue > 0) {
-    if (Math.floor(currentValue / 10) === 1 || currentValue % 10 < 1 || currentValue % 10 > 4) {
-      ending = endings[1];
-    } else if (currentValue % 10 > 1) {
-      ending = endings[2];
-    }
-    const textParts = [
-      lastValue > 0 ? ', ' : '',
-      `${currentValue} `,
-      word,
-      ending,
-    ];
-    result = textParts.reduce((acc, curr) => acc + curr);
-  }
-
-  return result;
-};
-
-// buttons state manager
-
-const adjustButtonsState = function optionsButtonsStateAccordingToMinAndMaxRanges(buttons, value, selector, leftOnly) {
-  const caseA = value < 1;
-  const caseB = value > 9;
-
-  if (caseA || caseB) {
-    if (caseA) {
-      buttons.left.classList.add(selector);
-    }
-    if (caseB && !leftOnly) {
-      buttons.right.classList.add(selector);
-    }
-  }
-  if (!(caseA || caseB)) {
-    buttons.left.classList.remove(selector);
-    buttons.right.classList.remove(selector);
-  }
-};
-
-// controllers actions
-
-const resetCounters = function resetCounterValueOnButtonClick(controllers, counters, buttonGroups) {
-  counters.forEach((counter) => counter.textContent = 0);
-
+function resetCounters(controllers, counters, buttonGroups) {
+  counters.forEach((counter) => {
+    const fixedCounter = counter;
+    fixedCounter.textContent = 0;
+  });
   buttonGroups.forEach((group) => {
     const buttons = defineButtons(group);
 
@@ -184,21 +148,75 @@ const resetCounters = function resetCounterValueOnButtonClick(controllers, count
   });
   defineResetControllerState(counters, controllers);
   updateDropdownValue([...counters][0], counters);
-};
+}
 
-const defineCountersSum = function (counters) {
-  const result = Array
-    .from(counters)
-    .map((counter) => Number(counter.textContent))
-    .reduce((acc, curr) => acc + curr);
+function updateDropdown(buttons, counter, addification, controllers, counters) {
+  const fixedCounter = counter;
+  const newCounterValue = parseInt(counter.textContent, 10) + addification;
 
-  return result;
-};
+  adjustButtonsState(buttons, newCounterValue, 'circle-button_frozen');
+  fixedCounter.textContent = newCounterValue;
+  updateDropdownValue(counter, counters);
 
-const acceptForm = function acceptFormOnButtonClick(controller) {
-  const frame = controller.closest('.input__element').querySelector('.input__frame');
+  if (controllers) {
+    defineResetControllerState(counters, controllers);
+  }
+}
 
-  frame.dispatchEvent(new Event('click'));
-};
+function initButtons(buttons, counters, controllers) {
+  const optionButtons = defineButtons(buttons);
+  const optionCounter = buttons.querySelector('.input__option-counter');
+  const optionCounterValue = optionCounter.textContent;
 
-find();
+  adjustButtonsState(optionButtons, optionCounterValue, 'circle-button_frozen');
+
+  const update = function update(elem, shift) {
+    const fixedElem = elem;
+
+    fixedElem.onclick = () => {
+      updateDropdown(optionButtons, optionCounter, shift, controllers, counters);
+    };
+  };
+  update(optionButtons.left, -1);
+  update(optionButtons.right, 1);
+}
+
+function initControllers(counters, controllers, buttonGroups) {
+  const resetController = controllers.left;
+  const acceptController = controllers.right;
+
+  defineResetControllerState(counters, controllers);
+
+  const resetControllerClickHandler = () => resetCounters(controllers, counters, buttonGroups);
+  const acceptControllerClickHandler = () => acceptForm(acceptController);
+
+  resetController.addEventListener('click', resetControllerClickHandler);
+  acceptController.addEventListener('click', acceptControllerClickHandler);
+}
+
+function initEventListeners(dropdown) {
+  const counters = dropdown.querySelectorAll('.input__option-counter');
+  const buttonGroups = dropdown.querySelectorAll('.input__option-button-block');
+  const controllersBar = dropdown.querySelector('.input__state-controllers');
+
+  updateDropdownValue(Array.from(counters)[0], counters);
+
+  if (controllersBar) {
+    const controllers = defineButtons(controllersBar);
+
+    initControllers(counters, controllers, buttonGroups);
+    buttonGroups.forEach((buttons) => initButtons(buttons, counters, controllers));
+  }
+
+  if (!controllersBar) {
+    buttonGroups.forEach((buttons) => initButtons(buttons, counters, undefined));
+  }
+}
+
+function findDropdownContainers() {
+  const dropdowns = document.querySelectorAll('.input__dropdown');
+
+  dropdowns.forEach((dropdown) => initEventListeners(dropdown));
+}
+
+findDropdownContainers();
