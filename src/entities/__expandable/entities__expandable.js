8bd9609dd659f21c32e47a5820662bae150bc152
Maxim Dejnew <red41tiger@mail.ru>
2022-01-18 12:48:13 +0700

    Added new method for shutting down already opened entities

--- src/entities/__expandable/entities__expandable.js
+++ src/entities/__expandable/entities__expandable.js
@@ -1,6 +1,7 @@
 import './entities__expandable.scss';
 
-function changeExpanderDisplayState(container, expander) {
+function changeExpanderDisplayState(container) {
+  const expander = container.querySelector('.js-expander');
   if (expander.hasAttribute('checked')) {
     expander.removeAttribute('checked');
   } else {
@@ -9,14 +10,25 @@ function changeExpanderDisplayState(container, expander) {
   container.classList.toggle('js-expander_active');
 }
 
+function closeEachActiveContainer(container) {
+  const currentActiveContainers = document.querySelectorAll('.js-expander_active');
+
+  currentActiveContainers.forEach((activeContainer) => {
+    if (!container.isEqualNode(activeContainer)) {
+      changeExpanderDisplayState(activeContainer);
+    }
+  })
+}
+
 function initEventListeners() {
   const containers = document.querySelectorAll('.js-expander__container');
 
   containers.forEach((container) => {
     const expander = container.querySelector('.js-expander');
 
-    const clickEventHandler = () => {
-      changeExpanderDisplayState(container, expander);
+    const clickEventHandler = function clickEventHandler() {
+      closeEachActiveContainer(container)
+      changeExpanderDisplayState(container);
     };
     expander.addEventListener('click', clickEventHandler);
   });
