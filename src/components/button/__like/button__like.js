6a437e0ff1fc4c7ef5d82007f731712acca78370
Maxim Dejnew <red41tiger@mail.ru>
2022-01-19 14:23:29 +0700

    Button-like is under reconstruction 3

--- src/components/button/__like/button__like.js
+++ src/components/button/__like/button__like.js
@@ -7,7 +7,7 @@ function likeButtonPressing({ target }) {
   const aim = target.closest('.like-button');
   const input = aim.firstElementChild;
   const icon = aim.querySelector('.material-icons');
-  const counter = aim.querySelector('.counter-elem');
+  const counter = aim.querySelector('.like-button__text-counter-elem');
   const caseInputChecked = input.hasAttribute('checked');
 
   if (caseInputChecked) {
